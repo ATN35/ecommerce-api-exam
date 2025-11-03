fc29ee7d2d5821c47819d1eaa9aa87f9994c952a
ATN <contact.atn35@gmail.com>
2025-11-03 15:08:11 +0100

    modification made to the route /me

--- ecommerce-api-exam/backend/src/routes/auth.js
+++ ecommerce-api-exam/backend/src/routes/auth.js
@@ -1,10 +1,20 @@
 const express = require('express');
-const { register, login, me } = require('../controllers/authController');
-const { authRequired } = require('../middlewares/auth');
 const router = express.Router();
+
+const { register, login } = require('../controllers/authController');
+const { requireAuth } = require('../middlewares/auth');
 
 router.post('/register', register);
 router.post('/login', login);
-router.get('/me', authRequired, me);
+
+router.get('/me', requireAuth, (req, res) => {
+  res.json({
+    user: {
+      id: req.user.sub,
+      email: req.user.email,
+      role: req.user.role,
+    },
+  });
+});
 
 module.exports = router;
