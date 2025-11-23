1127016e7e413b60b797cf88534648dcc8ab0f64
ATN <contact.atn35@gmail.com>
2025-11-23 18:15:43 +0100

    Addition of a folder linked directly to Postman to view my test collections, plus improvements regarding cookie files

--- backend/src/controllers/cookieController.js
+++ backend/src/controllers/cookieController.js
@@ -1,20 +1,28 @@
-const { pool } = require('../config/db')
-const Cart = require('../services/cartService')
+const { pool } = require('../config/db');
+const Cart = require('../services/cartService');
 
 async function deleteMyAccount(req, res, next) {
-  const userId = req.user.sub
-  const client = await pool.connect()
+  const userId = req.user.sub;
+  const client = await pool.connect();
+
   try {
-    await client.query('BEGIN')
-    await client.query('DELETE FROM users WHERE id=$1', [userId])
-    await client.query('COMMIT')
-    try { await Cart.clearCart(userId) } catch (_) {}
-    return res.status(204).end()
+    await client.query('BEGIN');
+    await client.query('DELETE FROM users WHERE id=$1', [userId]);
+    await client.query('COMMIT');
+
+    try {
+      await Cart.clearCart(userId);
+    } catch (_) {}
+
+    return res.status(204).end();
   } catch (err) {
-    try { await client.query('ROLLBACK') } catch (_) {}
-    return next(err)
+    try {
+      await client.query('ROLLBACK');
+    } catch (_) {}
+
+    return next(err);
   } finally {
-    client.release()
+    client.release();
   }
 }
 
@@ -22,9 +30,11 @@ function logout(_req, res) {
   res.clearCookie(process.env.COOKIE_NAME || 'token', {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'lax'
-  })
-  return res.status(204).end()
+    sameSite: process.env.NODE_ENV === 'production' ? 'strict' : 'lax',
+    path: '/',
+  });
+
+  return res.status(204).end();
 }
 
-module.exports = { deleteMyAccount, logout }
+module.exports = { deleteMyAccount, logout };
